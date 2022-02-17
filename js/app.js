7c1fbdc8f20e810dcc232fe59ed8ea1e62984b93
Racknar0 <racknarow1@gmail.com>
2022-02-17 17:28:31 -0500

    Mostrar carrito en el HTML

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Variables
 const carrito = document.querySelector("#carrito"); //!selección Carrito
-const contenedorCarrito = document.querySelector("lista-carrito tbody"); //!selección Body table
+const contenedorCarrito = document.querySelector("#lista-carrito tbody"); //!selección Body table
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito"); //!btn vaciar
 const listaCursos = document.querySelector("#lista-cursos"); //!lista de cursos
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -21,16 +22,47 @@ function agregarCurso(e) {
 function leerDatosCurso(curso) {
   // crear objeto con datos
 
-  console.log(curso);
-
   const infoCurso = {
-      imagen: curso.querySelector('img').src,
-      titulo: curso.querySelector('h4').textContent,
-      precio: curso.querySelector('.precio span').textContent,
-      id: curso.querySelector('a').getAttribute('data-id'),
-      cantidad: 1
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
 
-  }
+  //Agrega Elementos al arreglo
+  articulosCarrito = [...articulosCarrito, infoCurso];
 
-  console.log(infoCurso);
+  console.log(articulosCarrito);
+
+  carritoHTML();
+}
+
+//Muestra el carrito en el HTML
+function carritoHTML () {
+
+    //limpiar el html
+    limpiarHTML();
+
+    // itera el carrito y genera html
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('TR');
+        row.innerHTML = `
+        <td>
+            ${curso.titulo}
+        </td>
+        `;
+
+        //Agrega HTML al tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+//Elimina los curso del tbody
+function limpiarHTML() {
+    /* contenedorCarrito.innerHTML = ''; */
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
